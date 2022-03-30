0ffa7c6f3237430289bf6229407772ea9ea2e37f
Ivie Morrigan <iviehedera@gmail.com>
2022-03-30 10:07:01 +0100

    If there is an existing login.sh, wait

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 # If there is a running doormat process, wait for it to finish
 # In this way, we naively prevent multiple doormat login browser sessions
-while ps -ef | grep -v grep | grep -q "doormat login"
+while ps -ef | grep -v grep | grep -q "doormat login\|login.sh"
 do
 	echo "Waiting for previous doormat to finish"
 	sleep 5
