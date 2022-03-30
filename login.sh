e50ab69898d86e11b7370d0cf70505e23534b993
Ivie Morrigan <iviehedera@gmail.com>
2022-03-30 12:06:07 +0100

    Revert "Also check for existing login.sh processes, excluding the current script's PID"
    This reverts commit b95267fbedc742a84f2598271cd1f40f47fa9af3.

--- login.sh
+++ login.sh
@@ -2,9 +2,7 @@
 
 # If there is a running doormat process, wait for it to finish
 # In this way, we naively prevent multiple doormat login browser sessions
-#
-# Also check for existing login.sh processes, excluding the current script's PID
-while ps -ef | grep -v grep | grep -v $$ | grep -q "doormat login\|login.sh"
+while ps -ef | grep -v grep | grep -q "doormat login\|login.sh"
 do
 	echo "Waiting for previous doormat to finish"
 	sleep 5
