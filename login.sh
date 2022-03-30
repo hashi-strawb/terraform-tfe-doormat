dff6d49b52e8653d73b1779e11ba19fc5dc2211b
Lucy Davinhart <lucy@lucymhdavies.com>
2022-03-30 12:11:42 +0100

    Use a random sleep timer as a cheap hacky alternative to a mutex

--- login.sh
+++ login.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Sleep a random time to ensure we have only one instance of doormat running at a time
+# A proper mutex would be better, but this works okay
+sleep_timer=$(( ( RANDOM % 10 )  + 1 ))
+sleep ${sleep_timer}
+echo Sleeping ${sleep_timer}
+
 # If there is a running doormat process, wait for it to finish
 # In this way, we naively prevent multiple doormat login browser sessions
 while ps -ef | grep -v grep | grep -q "doormat login"
