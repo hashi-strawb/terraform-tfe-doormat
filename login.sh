86c1793c93268c4af1386d37fa658c2cea5e0e21
Ivie Morrigan <iviehedera@gmail.com>
2022-03-30 12:06:19 +0100

    Revert "If there is an existing login.sh, wait"
    This reverts commit 0ffa7c6f3237430289bf6229407772ea9ea2e37f.

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 # If there is a running doormat process, wait for it to finish
 # In this way, we naively prevent multiple doormat login browser sessions
-while ps -ef | grep -v grep | grep -q "doormat login\|login.sh"
+while ps -ef | grep -v grep | grep -q "doormat login"
 do
 	echo "Waiting for previous doormat to finish"
 	sleep 5
